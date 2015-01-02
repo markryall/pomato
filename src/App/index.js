608f6b9e7e87a32d46200eed90175ab5dc73faf6
Mark Ryall <mark@ryall.name>
2015-01-02 22:07:23 +1000

    switched to countdown with audio at the end

--- src/App/index.js
+++ src/App/index.js
@@ -7,17 +7,29 @@ module.exports = React.createClass({
   },
 
   tick: function() {
-    this.setState({time: this.state.time + 1});
+    this.setState({time: this.state.time - 1});
+    if (this.state.time == 0) {
+      this.refs.audio.getDOMNode().play();
+    } else {
+      setTimeout(this.tick, 1000);
+    }
   },
 
-  componentWillMount: function() {
-    setInterval(this.tick, 1000);
+  start: function() {
+    this.setState({time: 10});
+    setTimeout(this.tick, 1000);
   },
 
   render: function() {
     var number = ("00000" + this.state.time).slice(-5);
     return <div>
       <Lcd number={ number } />
+      <div>
+        <button value="Start" onClick={ this.start }>Start</button>
+      </div>
+      <audio ref="audio">
+        <source src="http://heroix-development.s3-website-us-east-1.amazonaws.com/boottime.mp3" type="audio/mpeg" />
+      </audio>
     </div>;
   }
 });
