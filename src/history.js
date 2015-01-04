47b583e08192a1a345f52b9f15c51a952271eeea
Mark Ryall <mark@ryall.name>
2015-01-05 00:17:02 +1000

    persisting counts to local storage

--- src/history.js
+++ src/history.js
@@ -4,7 +4,7 @@ var emitter = require('./emitter');
 module.exports = React.createClass({
   getInitialState: function() {
     var history = {
-      tasks: []
+      tasks: {}
     };
     if (window.localStorage['history']) {
       history = JSON.parse(window.localStorage['history']);
@@ -12,20 +12,29 @@ module.exports = React.createClass({
     return history;
   },
 
+  appendTask: function(data, completed) {
+    var tasks = this.state.tasks;
+    if (!tasks[data.task.name]) {
+      tasks[data.task.name] = { started: 0, completed: 0, time: 0 }
+    }
+    tasks[data.task.name]['started'] += 1
+    if (completed) {
+      tasks[data.task.name]['completed'] += 1
+    }
+    tasks[data.task.name]['time'] += data.time
+    this.setState({tasks: tasks});
+  },
+
   componentWillMount: function() {
     var component = this;
     emitter.on('startingTask', function(task) {
       console.log('starting', task);
     });
-    emitter.on('stoppingTask', function(task) {
-      var tasks = component.state.tasks;
-      tasks.push(task);
-      component.setState({tasks: tasks});
+    emitter.on('stoppingTask', function(data) {
+      component.appendTask(data, false);
     });
-    emitter.on('finishedTask', function(task) {
-      var tasks = component.state.tasks;
-      tasks.push(task);
-      component.setState({tasks: tasks});
+    emitter.on('finishedTask', function(data) {
+      component.appendTask(data, true);
     });
   },
 
