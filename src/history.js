42ebb81ae075f659c6bf67aac0bc2ba6d5a34d78
Mark Ryall <mark@ryall.name>
2015-01-05 00:04:11 +1000

    started persisting history of previous tasks

--- src/history.js
+++ src/history.js
@@ -2,20 +2,35 @@ var React = require('react');
 var emitter = require('./emitter');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    var history = {
+      tasks: []
+    };
+    if (window.localStorage['history']) {
+      history = JSON.parse(window.localStorage['history']);
+    }
+    return history;
+  },
+
   componentWillMount: function() {
     var component = this;
     emitter.on('startingTask', function(task) {
       console.log('starting', task);
     });
     emitter.on('stoppingTask', function(task) {
-      console.log('stopping', task);
+      var tasks = component.state.tasks;
+      tasks.push(task);
+      component.setState({tasks: tasks});
     });
     emitter.on('finishedTask', function(task) {
-      console.log('finished', task);
+      var tasks = component.state.tasks;
+      tasks.push(task);
+      component.setState({tasks: tasks});
     });
   },
 
   render: function() {
+    window.localStorage['history'] = JSON.stringify(this.state);
     return <p>history</p>;
   }
 });
